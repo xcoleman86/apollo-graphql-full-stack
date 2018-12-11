7a4c27142e4cfc70bedfaad7125c905b3b7b7cb6
Xavier Coleman <xcoleman86@icloud.com>
2018-12-11 07:03:56 -0500

    no message

---
+++ start/client/src/pages/login.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Mutation, ApolloConsumer } from 'react-apollo';
+import gql from 'graphql-tag';
+
+import LoginForm from '../components/login-form';
+
+const LOGIN_USER = gql`
+  mutation login($email: String!) {
+    login(email: $email)
+  }
+`;
+export default function Login() {
+    return (
+      <Mutation mutation={LOGIN_USER}>
+        {(login, { data }) => <LoginForm login={login} />}
+      </Mutation>
+    );
+  }
